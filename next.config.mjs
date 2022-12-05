5fa39459bb988175da12a637142db067db378ae5
Chris <37954566+codingnuclei@users.noreply.github.com>
2022-12-05 07:16:45 +0000

    feat: add pwa support

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,13 @@
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
-/** @type {import("next").NextConfig} */
+import withPWA from 'next-pwa';
+
+const myPWA = withPWA({
+  dest: 'public'
+})
+
+// /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   swcMinify: true,
@@ -14,4 +20,5 @@ const config = {
     defaultLocale: "en",
   },
 };
-export default config;
+
+export default myPWA(config);
